3243bec6011a787155b1bae070e0014e845016cc
caoguolin <kafeicgl@sina.com>
2024-04-09 16:00:40 +0800

    Update SubTask.py

--- High-throughput calculations framework/SubTask.py
+++ High-throughput calculations framework/SubTask.py
@@ -23,15 +23,11 @@ def SubTask(caltype,path):
     wlog = os.path.join(initpath,'log')   ##
     for i in range(len(steps)):
         step = steps[i]
-        # filepath = os.path.abspath('cal/filepath/{}/{}/'.format(caltype,step))
         filepath = os.path.abspath('commonfile/{}/{}/'.format(caltype,step))
         works = []
         for root,dirs,files in os.walk(path):
             works.append(dirs)
         works_num = works[0]
-        # tasks_name = []
-        # for j in range(len(works_num)):
-        #     tasks_name.append('87777order{}'.format(j+1))
 
         if caltype == 'slab':
             for j in range(len(works_num)):
@@ -41,13 +37,10 @@ def SubTask(caltype,path):
                     os.mkdir(workpath)
                 fpath = os.path.join(workpath,'OUTCAR')
                 if not os.path.exists(fpath):
-                    # os.system('cd {} ; cp INCAR POTCAR KPOINTS {} ; cd {}'.format(filepath,workpath,initpath))
                     os.system('cd {} ; cp INCAR POTCAR KPOINTS {} ; cp ../../vasp2.pbs {} ; cd {}'.format(filepath,workpath,workpath,initpath))
                     if step == 'step1':
-                        # os.system('cd {} ; mv HEA.vasp POSCAR ; mpirun -np 20 /public/software/vasp/vasp_std > log ; touch end.txt ; cd {}'.format(workpath,initpath))
                         os.system('cd {} ; mv HEA.vasp POSCAR ; qsub vasp2.pbs ; cd {}'.format(workpath,initpath))
                     elif step == 'step2':
-                        # os.system('cd {} ; cd ../step1/ ; cp CONTCAR ../step2/ ; cd ../step2/ ; mv CONTCAR POSCAR ; mpirun -np 20 /public/software/vasp/vasp_std > log ; touch end.txt ; cd {}'.format(workpath,initpath))
                         os.system('cd {} ; cd ../step1/ ; cp CONTCAR ../step2/ ; cd ../step2/ ; mv CONTCAR POSCAR ; qsub vasp2.pbs ; cd {}'.format(workpath,initpath))
                     time.sleep(10)
                     completed = False
@@ -61,18 +54,13 @@ def SubTask(caltype,path):
                             ff.close()
                         else:
                             time.sleep(10)
-                        # if os.path.exists(fpath):
-                        #     completed = True
-                        # else:
-                        #     time.sleep(10)
+
 
         elif caltype == 'adso':
         # elif caltype == 'adsoh':
             for j in range(len(works_num)):
                 task_name = works_num[j]
-                # sites = ['hollow','bridge','top']
-                sites = ['fixtopO']
-                # sites = ['fixtopO','fixbridgeO','fixhollowO','fixfcchollowO']
+                sites = ['fixtopO','fixbridgeO','fixhollowO','fixfcchollowO']
                 # sites = ['hollowOH','bridgeOH','topOH']
                 for k in range(len(sites)):
                     site = sites[k]
@@ -87,13 +75,10 @@ def SubTask(caltype,path):
                             os.mkdir(workpath)
                         fpath = os.path.join(workpath,'OUTCAR')
                         if not os.path.exists(fpath):
-                            # os.system('cd {} ; cp INCAR POTCAR KPOINTS {} ; cd {}'.format(filepath,workpath,initpath))
                             os.system('cd {} ; cp INCAR POTCAR KPOINTS {} ; cp ../../vasp.pbs {} ; cd {}'.format(filepath,workpath,workpath,initpath))     ##
                             if step == 'step1':
-                                # os.system('cd {} ; mpirun -np 20 /public/software/vasp/vasp_std > log ; touch end.txt ; cd {}'.format(workpath,initpath))
                                 os.system('cd {} ; qsub vasp.pbs ; cd {}'.format(workpath,initpath))
                             elif step == 'step2':
-                                # os.system('cd {} ; cd ../step1/ ; cp CONTCAR ../step2/ ; cd ../step2/ ; mv CONTCAR POSCAR ; mpirun -np 20 /public/software/vasp/vasp_std > log ; touch end.txt ; cd {}'.format(workpath,initpath))
                                 os.system('cd {} ; cd ../step1/ ; cp CONTCAR ../step2/ ; cd ../step2/ ; mv CONTCAR POSCAR ; qsub vasp.pbs ; cd {}'.format(workpath,initpath))
                             time.sleep(10)
                             completed = False
@@ -107,13 +92,9 @@ def SubTask(caltype,path):
                                     ff.close()
                                 else:
                                     time.sleep(10)
-                                # if os.path.exists(fpath):
-                                #     completed = True
-                                # else:
-                                #     time.sleep(10)
    
     return
 
-path = 'cal2/'    ##
+path = 'cal/'    ##
 caltype = 'adso'
 t = SubTask(caltype,path)
